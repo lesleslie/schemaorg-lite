65f77f50fe4c01535a8c1d11f523d3cf6768ac93
lesleslie <les@wedgwoodwebworks.com>
2023-03-19 10:45:40 -0700

    adjust logging

--- schemaorg/data/__init__.py
+++ schemaorg/data/__init__.py
@@ -1,7 +1,3 @@
-__author__ = "Vanessa Sochat"
-__copyright__ = "Copyright 2018-2021, Vanessa Sochat"
-__license__ = "MPL 2.0"
-
 import csv
 from pathlib import Path
 
--- schemaorg/main.py
+++ schemaorg/main.py
@@ -1,7 +1,3 @@
-__author__ = "Vanessa Sochat"
-__copyright__ = "Copyright 2018-2021, Vanessa Sochat"
-__license__ = "MPL 2.0"
-
 import re
 from contextlib import suppress
 
